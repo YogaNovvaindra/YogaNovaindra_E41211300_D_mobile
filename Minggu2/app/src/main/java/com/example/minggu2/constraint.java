bd775406c6937db48d07a5cfe8845b75fd6066f9
YogaNovvaindra <yoga.november2000@gmail.com>
2023-02-21 18:22:31 +0700

    fix Button

--- Minggu2/app/src/main/java/com/example/minggu2/constraint.java
+++ Minggu2/app/src/main/java/com/example/minggu2/constraint.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class customize extends AppCompatActivity {
+public class constraint extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customize);
+        setContentView(R.layout.activity_constraint);
     }
 }
