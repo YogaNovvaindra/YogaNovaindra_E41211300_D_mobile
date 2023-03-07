9cb17fbb73e838790329b911ff85537bbf9c1778
YogaNovvaindra <yoga.november2000@gmail.com>
2023-03-07 20:43:27 +0700

    add button

--- Minggu4/app/src/main/java/com/example/minggu4/Acara19.java
+++ Minggu4/app/src/main/java/com/example/minggu4/Acara19.java
@@ -2,54 +2,38 @@ package com.example.minggu4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class Acara19 extends AppCompatActivity {
-    Button btn1, btn2;
-
+    Button btnFirstFragment, btnSecondFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acara19);
-
-        btn1 = findViewById(R.id.btnfirstFragment);
-        btn2 = findViewById(R.id.btnsecondFragment);
-
-        btn1.setOnClickListener(new View.OnClickListener() {
+        btnFirstFragment = findViewById(R.id.firstFragment);
+        btnSecondFragment = findViewById(R.id.secondFragment);
+        btnFirstFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadFragment(new Acara19_firstFragment());
-
-                Toast.makeText(com.example.minggu4.Acara19.this, "ini adalah fragment pertama", Toast.LENGTH_SHORT).show();
             }
         });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
+        btnSecondFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loadFragment(new Acara19_secondFragment());
-
-                Toast.makeText(com.example.minggu4.Acara19.this, "ini adalah fragment kedua", Toast.LENGTH_SHORT).show();
             }
         });
     }
-
-    private void loadFragment(Acara19_firstFragment firstFragmentActivity) {
+    private void loadFragment(Fragment fragment) {
         FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.container, new Acara19_firstFragment());
-        ft.commit();
-    }
-
-    private void loadFragment(Acara19_secondFragment firstFragmentActivity) {
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.container, new Acara19_secondFragment());
-        ft.commit();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, fragment);
+        fragmentTransaction.commit();
     }
 }
