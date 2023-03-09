6fd3a6075019af877a13eca212d7b7b2aa1e802f
YogaNovvaindra <yoga.november2000@gmail.com>
2023-03-09 14:14:10 +0700

    Minggu 5

---
+++ Minggu5/app/src/main/java/com/example/minggu5/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.minggu5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button14, button15;
+
+    private void initComponent() {
+        button14  = (Button) findViewById(R.id.button_22);
+        button15  = (Button) findViewById(R.id.button_23);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        initComponent();
+
+        button14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,IntentImplicit.class);
+                startActivity(intent);
+            }
+        });
+        button15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, IntentExplicit.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
